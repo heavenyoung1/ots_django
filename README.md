aec445cf1b63dc2723fecba55bb192dac9c433d3
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-11-28 20:58:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# django_password_generator
+Данный (первый из трёх) проект выполнен в рамках Курса **'Django  - Full Stack разработка веб-сайтов на Python'** и
+представляет собой веб-сайт для генерации случайных паролей.
+Стек использованных знаний:
+- **Django 4.0.6**
+- **Bootstrap**
+
+При помощи Djangо  я создал веб-сайт, генерирующий случайные пароли по заданным критериям (чекбоксы, например использование специальных символов, цифр или больших букв)
+
+Страница стилизована при помощи  Bootstrap 5.2.0
+
+Для запуска проекта:
+1. Клонируйте репозиторий локально.
+2. Запустите сервер с Django командой
+` python manage.py runserver `
+![text](https://github.com/heavenyoung1/django_password_generator/blob/master/home_page.png 'home_page')
+![text](https://github.com/heavenyoung1/django_password_generator/blob/master/generated_password.png 'generated password')
+
+
---
+++ generator/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def description(request):
+    return render(request, 'generator/description.html')
+
+def password(request):
+
+
+    characters = list('abcdefghiklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTVUWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()"№;:?-='))
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    length = int(request.GET.get('length', 12))
+
+
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password':thepassword})
+
+# Create your views here.
